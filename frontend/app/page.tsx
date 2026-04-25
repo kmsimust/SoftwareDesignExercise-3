2930bf0f01cb6a62c44fe12ed8cf0d26f0029043
kmsimust <phubet.u@ku.th>
2026-04-25 14:35:42 +0700

    README update 2

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,9 +1,28 @@
-import Image from "next/image";
+"use client";
+
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+import Link from 'next/link';
 
 export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    const user = localStorage.getItem('user');
+    if (user) {
+      router.push('/library');
+    }
+  }, [router]);
+
   return (
     <div>
-      Ya, It's landing page.
+      <h1>Ya, It's landing page.</h1>
+      <Link href="/signup">
+        <button>Sign Up</button>
+      </Link>
+      <Link href="/login">
+        <button>Login</button>
+      </Link>
     </div>
   );
 }
